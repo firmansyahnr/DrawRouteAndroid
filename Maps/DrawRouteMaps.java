2e92b0f551798ff4c1799d7d88cf8ecc0f9c2699
firmanrpl <firmanrpl@gmail.com>
2017-01-28 15:46:29 +0700

    Draw Route

---
+++ Maps/DrawRouteMaps.java
@@ -0,0 +1,34 @@
+package com.dycode.tracking.Maps;
+
+import android.content.Context;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by FIRMANRPL on 1/21/2017.
+ */
+
+public class DrawRouteMaps {
+
+    private static DrawRouteMaps instance;
+    private Context context;
+
+    public static DrawRouteMaps getInstance(Context context) {
+        instance = new DrawRouteMaps();
+        instance.context = context;
+        return instance;
+    }
+
+    public DrawRouteMaps draw(LatLng origin, LatLng destination, GoogleMap googleMap){
+        String url_route = FetchUrl.getUrl(origin, destination);
+        DrawRoute drawRoute = new DrawRoute(googleMap);
+        drawRoute.execute(url_route);
+        return instance;
+    }
+
+    public static Context getContext() {
+        return instance.context;
+    }
+}
+
